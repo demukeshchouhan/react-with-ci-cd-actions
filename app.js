51a5412cb0f2b85c5cf911d2b10c26c71beedd59
Mukesh Chauhan <demukeshchouhan@users.noreply.github.com>
2024-03-18 17:09:11 +0530

    breaking test pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 app.use((req, res) => {
-  .log(process.env.MONGODB_URL);
+  console.log(process.env.MONGODB_URL);
   res.send("This is an express app");
 });
 app.listen(3300, () => console.log("listening on port 3300"));
